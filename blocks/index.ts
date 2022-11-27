81c7e92610f6056a8c670e64092f07df0d9e2864
Alexandre <a.tanyeres@gmail.com>
2022-11-28 00:05:47 +0100

    few blocks added

--- blocks/index.ts
+++ blocks/index.ts
@@ -1,7 +1,6 @@
 import Block1_1Banner from './Block1_1Banner'
 import Block1_1navBar from './Block1_1navBar'
 import Block1_2 from './Block1_2'
-import Block1_3 from './Block1_3'
 import Block1_4 from './Block1_4'
 
 import Block2_2 from './Block2_2'
@@ -9,7 +8,6 @@ import Block2_2 from './Block2_2'
 import Block3_2amap from './Block3_2amap'
 import Block3_2icons from './Block3_2icons'
 import Block3_3 from './Block3_3'
-import Block3_4 from './Block3_4'
 import Block3_5 from './Block3_5'
 
 import Block4_2 from './Block4_2'
@@ -27,52 +25,59 @@ import Block5_5_2 from './Block5_5_2'
 
 import Block6_2_1 from './Block6_2_1'
 import Block6_2_2 from './Block6_2_2'
-import Block6_2_3 from './Block6_2_3'
-import Block6_3 from './Block6_3'
+import TitleText2Colonnes from './TitleText2Colonnes'
+
+import TexteMedia3 from './TexteMedia3'
+import ListIcons from './ListIcons'
+import Image from './Image'
+import SimpleBanner from './SimpleBanner'
+import Banner2images from './Banner2images'
+import Banner3images from './Banner3images'
+import TexteMedia1 from './TexteMedia1'
+import TexteMedia2 from './TextMedia2'
 
 const blocks = {
     // Title,
     // Grid,
     // Display,
     // Navigation,
-
     // SimpleTitle,
     // SimpleImage,
     // SimpleBanner,
     // SimpleButton,
     // SimpleForm,
-
     Block1_1Banner,
     Block1_1navBar,
     Block1_2,
-    Block1_3,
-    Block1_4,
-
     Block2_2,
-
     Block3_2amap,
     Block3_2icons,
     Block3_3,
-    Block3_4,
     Block3_5,
-
     Block4_2,
     Block4_3_1,
     Block4_3_2,
     Block4_4_1,
     Block4_4_2,
-
     Block5_2_1,
     Block5_2_2,
     Block5_3,
     Block5_4,
     Block5_5_1,
     Block5_5_2,
-
     Block6_2_1,
     Block6_2_2,
-    Block6_2_3,
-    Block6_3,
+
+    Block1_4,
+    TitleText2Colonnes,
+    TexteMedia1,
+    TexteMedia2,
+    TexteMedia3,
+    ListIcons,
+    Image,
+    SimpleBanner,
+    Banner2images,
+    Banner3images,
 }
 
 export default blocks
